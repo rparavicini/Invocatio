83ac665f30b0d882342dd56ddac57d35adf8c8c2
Roman Paravicini <rparavicini@gmail.com>
2015-03-31 12:37:13 +0200

    added some more features

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@
 var creature = {
 	summon: {},
 	control: {},
-	costs: {}
+	costs: {},
+};
+var summoner = {
+	zfw: {
+		base: 11,
+	},
 };
 var modifiers = {
-	truename: 0
+	truename: 0,
+	outfit: 0,
+	candle: 0,
+	chalk: 0,
+	sword: 0,
+	integra: 0,
+	circle: 0,
 };
 var xml = '';
 
@@ -62,6 +73,9 @@ $(function() {
 			if (el.attr('data-relation') == 'creature') {
 				var id = el.attr('id').split('_');
 				creature[id[0]][id[1]] = val;
+			} else if (el.attr('data-relation') == 'summoner') {
+				var id = el.attr('id').split('_');
+				summoner[id[0]][id[1]] = val;
 			} else {
 				modifiers[el.attr('id')] = val;
 			}
@@ -78,7 +92,8 @@ $(function() {
 			var el = $(this).find(':selected');
 
 			// get data
-			var cdata = xml.find('creaturegroup:eq(' + el.attr('data-group') + ')').find('creature:eq(' + el.attr('data-creature') + ')');
+			var gdata = xml.find('creaturegroup:eq(' + el.attr('data-group') + ')');
+			var cdata = gdata.find('creature:eq(' + el.attr('data-creature') + ')');
 
 			if (cdata.length !== 0) {
 
@@ -92,7 +107,37 @@ $(function() {
 				creature.control.base = cdata.attr('control');
 				creature.costs.base = cdata.attr('costs');
 
+				// set creature type select
+				var ctype = gdata.attr('creaturetype');
+				$('#creature_type').val(ctype).selectmenu('refresh');;
+
 			}
+
+		})
+		// set values for radio change
+		.on('change', '[type="radio"]', function(e) {
+
+			// init element
+			var el = $(this);
+
+			// get value of element
+			var val = el.val();
+
+			// set variable to value of element
+			modifiers[el.attr('name')] = val;
+
+		})
+		// set values for checkbox change
+		.on('change', '[type="checkbox"]', function(e) {
+
+			// init element
+			var el = $(this);
+
+			// get value of element
+			var val = ( el.is(':checked') ? 1 : 0 );
+
+			// set variable to value of element
+			modifiers[el.attr('id')] = val;
 
 		})
 		// on pagechange
@@ -102,14 +147,99 @@ $(function() {
 			if (page_id == 'summary') {
 
 				$.each(modifiers, function(key,value) {
+					if (value == 0) {
+						delete creature.summon[key];
+						delete creature.control[key];
+						return true;
+					}
 					switch (key) {
 						case 'truename':
-							creature.summon.truename = value * -1;
-							creature.control.truename = Math.round(value/-3);
+							creature.summon[key] = value * -1;
+							creature.control[key] = Math.round(value/-3);
+							break;
+						case 'outfit':
+						case 'sword':
+							creature.summon[key] = value * -1;
+							creature.control[key] = value * -1;
+							break;
+						case 'integra':
+							summoner.zfw[key] = 7;
+							break;
+						case 'candle':
+							switch (value) {
+								case 'na':
+								case 'a':
+									delete creature.summon[key];
+									delete creature.control[key];
+									break;
+								case 'b':
+									creature.summon[key] = -1;
+									break;
+								case 'c':
+									creature.control[key] = -1;
+									break;
+								case 'd':
+									creature.summon[key] = -1;
+									creature.control[key] = -1;
+									break;
+								case 'e':
+									creature.summon[key] = -2;
+									creature.control[key] = -2;
+									break;
+								case 'f':
+									creature.summon[key] = -3;
+									creature.control[key] = -3;
+									break;
+							}
+							break;
+						case 'chalk':
+							switch (value) {
+								case 'na':
+								case 'b':
+									delete creature.summon[key];
+									delete creature.control[key];
+									break;
+								case 'a':
+								case 'c':
+									creature.summon[key] = -1;
+									break;
+								case 'd':
+									creature.summon[key] = -1;
+									creature.control[key] = -1;
+									break;
+								case 'e':
+									creature.summon[key] = -2;
+									creature.control[key] = -1;
+									break;
+								case 'f':
+									creature.summon[key] = -3;
+									creature.control[key] = -2;
+									break;
+							}
+							break;
+						case 'circle':
+							creature.summon[key] = value * -1;
+							creature.control[key] = (value * -1) - 3;
 							break;
 					}
 				})
-console.log(creature);
+
+				$.each(summoner, function(key,value) {
+					var sum = 0;
+					var detail = '';
+					$.each(value, function(k,v) {
+						x = parseInt(v);
+						if (x < 0 || k == 'base') {
+							detail += v;
+						} else {
+							detail += '+' + v;
+						}
+						sum += x;
+					})
+					$('#summary #summary_' + key + '_detail').html(detail);
+					$('#summary #summary_' + key).html(sum);
+				});
+
 				$.each(creature, function(key,value) {
 					var sum = 0;
 					var detail = '';
